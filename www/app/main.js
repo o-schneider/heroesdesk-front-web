703bfe8232c040611bcb353d8ea289f1d2dc310b
tdeheurles <tdeheurles@gmail.com>
2015-03-23 21:25:01 +0100

    Exemple React - Exemple de Composants

--- www/app/main.js
+++ www/app/main.js
@@ -4,48 +4,65 @@
 //var Bootstrap = require('react-bootstrap');
 //var bootstrap = require('bootstrap');   // On devra passer via React-bootstrap si on utilise React
 
+var MainNavbar = React.createClass({
+    render: function(){
+        return  <ul className="nav navbar-nav navbar-left">
+                    <li className="active"><a href="index.html">Desk</a></li>
+                    <li><a href="#">Search</a></li>
+                    <li><a href="#">Projects</a></li>
+                    <li><a href="#">Activity</a></li>
+                </ul>
+    }
+}) ;
+
+var Login = React.createClass({
+    render: function() {
+        return  <ul className="nav navbar-nav navbar-right">
+                    <li className="dropdown">
+                        <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">HerosName<span className="caret"></span></a>
+                        <ul className="dropdown-menu" role="menu">
+                            <li><a href="#">Account</a></li>
+                            <li className="divider"></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+    }
+});
+
+var Search = React.createClass({
+    render: function() {
+        return  <form className="navbar-form navbar-right" role="search">
+                    <div classNmae="form-group">
+                        <input type="text" className="form-control" placeholder="Search"/>
+                        <button type="submit" className="btn btn-default"><i className="fa fa-search"></i></button>
+                    </div>
+                </form>;
+    }
+});
+
 var HeroesHeader = React.createClass({
     render: function() {
         return  <header role="banner">
-            <nav role="navigation" className="navbar navbar-static-top navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                        <a className="navbar-brand navbar-left" href="index.html">Heroes Desk</a><p className="navbar-text visible-xs-block">Desk (todo wire through React)</p>
-                    </div>
-                    <div className="navbar-collapse collapse">
-
-                        <ul className="nav navbar-nav navbar-left">
-                            <li className="active"><a href="index.html">Desk</a></li>
-                            <li><a href="#">Search</a></li>
-                            <li><a href="#">Projects</a></li>
-                            <li><a href="#">Activity</a></li>
-                        </ul>
-                        <ul className="nav navbar-nav navbar-right">
-                            <li className="dropdown">
-                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">HerosName<span className="caret"></span></a>
-                                <ul className="dropdown-menu" role="menu">
-                                    <li><a href="#">Account</a></li>
-                                    <li className="divider"></li>
-                                    <li><a href="#">Logout</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-
-                        <form className="navbar-form navbar-right" role="search">
-                            <div classNmae="form-group">
-                                <input type="text" className="form-control" placeholder="Search"/>
-                                <button type="submit" className="btn btn-default"><i className="fa fa-search"></i></button>
+                    <nav role="navigation" className="navbar navbar-static-top navbar-default">
+                        <div className="container-fluid">
+                            <div className="navbar-header">
+                                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                                    <span className="icon-bar"></span>
+                                    <span className="icon-bar"></span>
+                                    <span className="icon-bar"></span>
+                                </button>
+                                <a className="navbar-brand navbar-left" href="index.html">Heroes Desk</a><p className="navbar-text visible-xs-block">Desk (todo wire through React)</p>
                             </div>
-                        </form>
-                    </div>
-                </div>
-            </nav>
-        </header>;
+                            <div className="navbar-collapse collapse">
+
+                                <MainNavbar />
+                                <Login />
+                                <Search />
+                            </div>
+                        </div>
+                    </nav>
+                </header>;
 
     }
 });
