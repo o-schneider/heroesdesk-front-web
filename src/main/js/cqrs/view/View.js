94e0b7088caecbd2f47fa61e1f953f21b4fdf6f6
jpachod <joseph.pachod@berger-levrault.fr>
2015-08-07 08:33:59 +0200

    add view's subscription

--- src/main/js/cqrs/view/View.js
+++ src/main/js/cqrs/view/View.js
@@ -2,10 +2,28 @@
 
 import '../event/EventBus';
 import {check} from '../../check/check';
+import _ from 'lodash';
 
 export default class View {
   constructor(eventBus, listenedEventTypes) {
-    check.notNull({'eventBus' : eventBus, 'listenedEventTypes' : listenedEventTypes});
+    check.notNull({'eventBus': eventBus, 'listenedEventTypes': listenedEventTypes});
+    this.eventBus = eventBus;
+    this.listenedEventTypes = listenedEventTypes;
+    this.init();
   }
 
+  init() {
+
+    const eventBus = this.eventBus;
+    _.forEach(this.listenedEventTypes, function (eventType) {
+      eventBus.subscribe(eventType, function (message) {
+          on(eventType, message);
+      });
+
+    });
+  }
+
+  on(eventType, message){
+
+  }
 }
--- src/test/unit/cqrs/view/ViewTest.js
+++ src/test/unit/cqrs/view/ViewTest.js
@@ -17,5 +17,23 @@ describe('View', function () {
       new View();
     });
   });
+
+  it("subscribe given event type", function () {
+    class FakeEventBus extends EventBus {
+      constructor(collector){
+        super();
+        this.collector = collector;
+      }
+      subscribe(messageType, callback){
+        console.log("message type " + messageType + ", callback " + callback);
+        this.collector.push({messageType,callback});
+      }
+    }
+    const collector = new Array();
+    const fakeEventBus = new FakeEventBus(collector);
+    const listenedEventTypes = ["messageType"];
+    new View(fakeEventBus,listenedEventTypes);
+    assert.equal(1,collector.length);
+  });
 });
 
